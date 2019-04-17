9efb22115c31e5e0c7cdbaa993e521181edab985
Jean-Michel "Meesh <mich0391@umn.edu>
2019-04-16 20:32:55 -0500

    Create Transgene_Map_Call.sh

---
+++ Transgene_Map_Call.sh
@@ -0,0 +1,209 @@
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-14_74_S6_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-14_74_S6_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT579.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-22_80_S5_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-22_80_S5_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT579.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-27_84_S7_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-27_84_S7_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT579.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-39_89_S8_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt0579-39_89_S8_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT579.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt602-1-1_S2_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt602-1-1_S2_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT602.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt602-1_S1_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt602-1_S1_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT602.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt641-F-3-5_220_S4_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt641-F-3-5_220_S4_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT641.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt642-L-9-2_232_S3_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170914_D00635_0282_BCBKV4ANXX/Stupar_Project_044/Wpt642-L-9-2_232_S3_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT642.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt519-1_S16_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt519-1_S16_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT519.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt527-1_S8_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt527-1_S8_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT527.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt527-2_S9_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt527-2_S9_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT527.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt536-2-13-15_S12_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt536-2-13-15_S12_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT536.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt536-2-13-16_S11_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt536-2-13-16_S11_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT536.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt536-2_S10_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt536-2_S10_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT536.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt553-6-11_S15_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt553-6-11_S15_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT553.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt553-6-8_S14_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt553-6-8_S14_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT553.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt553-6_S13_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt553-6_S13_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT553.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-1-1_S2_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-1-1_S2_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT590.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-1-2_S3_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-1-2_S3_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT590.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-1_S1_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-1_S1_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT590.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
+
+TransGeneMap.sh \
+    -f /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-2_S7_R1_001.fastq \
+    -r /panfs/roc/data_release/1/umgc/stuparr/hiseq/170118_D00635_0195_ACAG5GANXX/Stupar_Project_040/Wpt590-2_S7_R2_001.fastq \
+    -s /panfs/roc/groups/13/stuparr/mich0391/Software/Trimmomatic-0.36 \
+    -t 8 \
+    -u /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    -v /panfs/roc/groups/13/stuparr/mich0391/Projects/CRISPR_WPT/Transgene/FullVector/WPT590.fa \
+    -i True \
+    -o /panfs/roc/scratch/michnoj0/CRISPR_WPT/Results/TG
